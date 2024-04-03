b57bfd41007a07080f4fe908b2544d241969be91
PrasadCoder <prasadsethi007@gmail.com>
2024-04-03 13:06:11 +0530

    Add initial project files

---
+++ src/test/java/com/mycompany/crmapp/CrmappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mycompany.crmapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrmappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
